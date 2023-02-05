3cb0eee41a36dc612cf78785370c8e7eda80604e
Giratina118 <rmh118@naver.com>
2023-02-05 11:50:57 +0900

    Csharp Unity class project file

---
+++ CSharpStudy/2주차/CSharpStudy2_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //1. 이름을 입력받기
+            string name;
+            System.Console.Write("이름을 입력해 주세요: ");
+            name = System.Console.ReadLine();
+
+            //2. 출생년도 입력받기
+            string birthyear;
+            int age;
+            System.Console.Write("출생년도를 입력해 주세요: ");
+            birthyear = System.Console.ReadLine();
+            age = 2024 - System.Convert.ToInt32(birthyear);
+            
+            //3. 자기소개 입력받기
+            string introduce;
+            System.Console.Write("자기소개를 입력해 주세요: ");
+            introduce = System.Console.ReadLine();
+            
+            //4. WriteLine으로 출력하기(출생년도 -> 나이로)
+            System.Console.WriteLine("\n\n이름은 {0}입니다.", name);
+            System.Console.WriteLine("나이는 {0}살 입니다.", age);
+            System.Console.WriteLine("자기소개: {0}\n\n", introduce);
+
+            //5. $넣어서 표현하기
+            System.Console.WriteLine($"이름은 {name}입니다.");
+            System.Console.WriteLine($"나이는 {age}살 입니다.");
+            System.Console.WriteLine($"자기소개: {introduce}\n");
+            
+            //6. @넣어서 사각형 그리기
+            System.Console.WriteLine(@"
+ㅁㅁㅁㅁ
+ㅁㅁㅁㅁ
+ㅁㅁㅁㅁ
+ㅁㅁㅁㅁ");
+            
+            //7. 1, 2, 3 string배열에 넣기
+            string[] information = new string[3] { name, age.ToString(), introduce };
+
+            //8. string 배열값을 4번과 같은 방식으로 출력하기
+            System.Console.WriteLine("\n\n이름은 {0}입니다.", information[0]);
+            System.Console.WriteLine("나이는 {0}살 입니다.", information[1]);
+            System.Console.WriteLine("자기소개: {0}\n\n", information[2]);
+
+            //9. string 배열값을 5번과 같은 방식으로 출력하기
+            System.Console.WriteLine($"이름은 {information[0]}입니다.");
+            System.Console.WriteLine($"나이는 {information[1]}살 입니다.");
+            System.Console.WriteLine($"자기소개: {information[2]}");
+            */
+
+        }
+    }
+}
